134f2b11f6184462450eab84aabce260599a70d1
Shina Park <shina820@gmail.com>
2020-03-09 20:26:24 -0700

    Edit Function

--- src/app/add-grocery/add-grocery.component.ts
+++ src/app/add-grocery/add-grocery.component.ts
@@ -5,22 +5,53 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './add-grocery.component.html',
   styleUrls: ['./add-grocery.component.css']
 })
-export class AddGroceryComponent implements OnInit {
-  task: string; // item
-  tasks = []; // initial status empty
 
-  // add item to the tasks 
+export class AddGroceryComponent implements OnInit {
+  // define each task(a single grocery item) with the name and id
+  task = {   
+    name: '',
+    id: 0
+  }; 
+  // initial tasks(grocery list) status: "empty"
+  tasks = []; 
+
+  // add an item to the tasks array
   onAdd() {
-    this.tasks.push({ name: this.task });
-    this.task='';
+    if (this.task.id === 0) {
+      // assign an unique timestamp to the id
+      this.tasks.push({ name: this.task.name, id:(new Date()).getTime()});
+    }
+
+    // reset task
+    this.task = {
+      name: '',
+      id: 0
+    }
   }
 
+  // strike item
+  onStrike(index: number) {
+      if (this.tasks.indexOf(index)) {
+        // apply strike class. default "false"
+        if (this.tasks[index].strike) {
+          this.tasks[index].strike = false;
+        } else {
+          this.tasks[index].strike = true;
+        }
+      }   
+  }
+
+  // edit item
+  onEdit(item) {
+    this.task = item;
+  }
+  
   // delete item
-  onDelete(index) {
+  onDelete(index: number) {
     this.tasks.splice(index, 1)
   }
 
-  // delete all item in the tasks
+  // delete ALL item 
   deleteAll() {
     this.tasks = [];
   }
